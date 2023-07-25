bcea8bc8991e55ae51434b81851d8cc845a0dbc5
giovarsu <giovanny.arango@cadena.com.co>
2023-07-25 17:02:40 -0500

    add interceptor in axios hooks

--- src/hooks/useAxiosFetch.tsx
+++ src/hooks/useAxiosFetch.tsx
@@ -1,10 +1,12 @@
 import { AuthContext } from "@/context/auth/AuthContext";
+import { HelperContext } from "@/context/helper/HelperContext";
 import axios from "axios";
 import { useContext } from "react";
 
 export const useAxiosFetch = () => {
 
-    const { state } = useContext(AuthContext);
+    const { state, logOut } = useContext(AuthContext);
+    const { hideLoader } = useContext(HelperContext);
     const { token } = state;
 
     const todosApi = axios.create({
@@ -13,6 +15,22 @@ export const useAxiosFetch = () => {
             'x-token' : token
         }
     })
+
+    todosApi.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if ( error.response && error.response.status === 401 ) {
+                window.alert('Session expired');
+                logOut();
+            }
+
+            hideLoader()
+
+            return Promise.reject(error);
+        }
+    )
 
     return {
         todosApi
